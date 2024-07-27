dc5334cd1f20673cbad4145c8d2a38a873459fcc
GilangDjati <111202113919@mhs.dinus.ac.id>
2024-07-27 08:04:14 +0700

    Sholatobu

---
+++ native/technoid/user/tentang.php
@@ -0,0 +1,28 @@
+<?php
+require '../config/config.php';
+
+
+require 'templates/header.php';
+?>
+
+<div class="row bg-white border p-2 mt-5">
+    <div class="col-md ml-3">
+        <h1>Tentang Kami</h1>
+        <p class="lead">Sholatobu memberdayakan jutaan bisnis di seluruh dunia. Platform perdagangan lengkap untuk memulai, menjalankan, dan mengembangkan bisnis. Membuat perdagangan menjadi lebih baik bagi semua orang. Kami membantu orang-orang mencapai kemandirian dengan mempermudah memulai, menjalankan, dan mengembangkan bisnis. Kami percaya bahwa masa depan perdagangan memiliki lebih banyak suara, bukan lebih sedikit, jadi kami mengurangi hambatan kepemilikan bisnis untuk menjadikan perdagangan lebih baik bagi semua orang.
+        </p>
+        <hr>
+        <div class="row ml-1">
+            <div class="w-100">
+                <h3><i class="fa fa-map-marker"></i>Alamat</h3>
+                <p>Semarang Barat<br>Kota Semarang<br>Jawa Tengah<br>Indonesia<br><strong>Sholatobu</strong></p>
+            </div>
+            <!-- /.col-sm-4-->
+            <div class="">
+                <h3><i class="fa fa-phone"></i> Hubungi Kami</h3>
+                <p class="text-muted">Anda dapat menghubungi kami di nomor yang tertera dibawah ini</p>
+                <p><strong>+62 1234 5678 987</strong></p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require 'templates/footer.php' ?>
